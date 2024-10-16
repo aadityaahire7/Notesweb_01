a8f827dfc4f1ab85376486379b0c737588cbadd8
aadityaahire <aadityaahire12@gmail.com>
2024-10-17 01:22:00 +0530

    ami check

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "notesapp" {
-  ami             = "ami-00f251754ac5da7f0"  # Amazon Linux 2 AMI
+  ami             = "ami-0d1622042e957c247"  # Amazon Linux 2 AMI
   instance_type   = var.instance_type
   key_name        = var.key_name
 
