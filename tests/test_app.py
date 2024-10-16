86eb0f066cb887c20ba9e0789fe432a6c0c090fc
Anujesh Ansh <anshanujesh@gmail.com>
2024-10-16 22:40:59 +0530

    SonarQube Update

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
 import pytest
+import sys
+import os
+
+# Add the root directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from website import create_app
 
 @pytest.fixture
